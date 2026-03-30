73a5ee3d6f8ea97044ad86e42aa09f35edf0a327
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2026-03-30 18:40:41 +0530

    add Topbar component

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -4,13 +4,12 @@ export default function Topbar() {
   return (
     <div className="flex justify-between items-center bg-white p-4 rounded-xl shadow">
       
-      {/* Left - Title */}
+     
       <h2 className="text-xl font-semibold">Dashboard</h2>
 
-      {/* Right - Search + Icons + Profile */}
+   
       <div className="flex items-center gap-4">
         
-        {/* Search Box */}
         <div className="flex items-center bg-gray-100 px-3 py-2 rounded-lg">
           <FaSearch className="text-gray-500" />
           <input
@@ -20,10 +19,10 @@ export default function Topbar() {
           />
         </div>
 
-        {/* Notification Icon */}
+     }
         <FaBell className="text-gray-600 cursor-pointer" />
 
-        {/* Profile */}
+       
         <div className="flex items-center gap-2">
           <img
             src="https://i.pravatar.cc/40"
